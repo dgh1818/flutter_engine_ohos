b552f491c72b725e5975cfc47a8b408b759a66e7
cpl <lichongpeng@huawei.com>
2024-10-28 09:38:35 +0000

    无障碍: 字体粗细适配 * 删除注释代码 * refactor:完善代码规范，补充必要注释 * fix:完善部分代码规范 * feat:无障碍字体粗细需求开发

--- engine/src/flutter/shell/platform/ohos/library_loader.cpp
+++ engine/src/flutter/shell/platform/ohos/library_loader.cpp
@@ -152,6 +152,17 @@ static napi_value Init(napi_env env, napi_value exports) {
       DECLARE_NAPI_FUNCTION(
           "nativeSetTextureBackGroundPixelMap",
           flutter::PlatformViewOHOSNapi::nativeSetTextureBackGroundPixelMap),
+
+      DECLARE_NAPI_FUNCTION(
+          "nativeUpdateSemantics",
+          flutter::PlatformViewOHOSNapi::nativeUpdateSemantics),
+      DECLARE_NAPI_FUNCTION("nativeUpdateCustomAccessibilityActions",
+                            flutter::PlatformViewOHOSNapi::
+                                nativeUpdateCustomAccessibilityActions),
+      DECLARE_NAPI_FUNCTION(
+          "nativeSetFontWeightScale",
+          flutter::PlatformViewOHOSNapi::nativeSetFontWeightScale),
+
       DECLARE_NAPI_FUNCTION(
           "nativeLookupCallbackInformation",
           flutter::PlatformViewOHOSNapi::nativeLookupCallbackInformation),
