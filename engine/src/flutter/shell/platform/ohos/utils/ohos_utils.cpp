321ef0658d40b922eeae869531aee3c5db7b145f
zjxi <xizijun1@huawei.com>
2024-12-17 21:10:05 +0800

    feat:增加selecttext动作处理逻辑，新增map序列化字节数组逻辑
    Signed-off-by: zjxi <xizijun1@huawei.com>

--- engine/src/flutter/shell/platform/ohos/utils/ohos_utils.cpp
+++ engine/src/flutter/shell/platform/ohos/utils/ohos_utils.cpp
@@ -18,4 +18,42 @@ namespace flutter {
 OHOSUtils::OHOSUtils() {};
 OHOSUtils::~OHOSUtils() {};
 
+/**
+ * 将string序列化为uint8字节数组
+ */
+void OHOSUtils::SerializeString(const std::string& str, std::vector<uint8_t>& buffer)
+{
+    // store the length of the string as a uint8_t
+    uint32_t length = str.size();
+    buffer.insert(buffer.end(), 
+                  reinterpret_cast<uint8_t*>(&length), 
+                  reinterpret_cast<uint8_t*>(&length) + sizeof(length));
+    // store the actual string data
+    buffer.insert(buffer.end(), str.begin(), str.end());
+}
+
+/**
+ * 将map<string, int>序列化为uint8字节数组
+ */
+std::vector<uint8_t> OHOSUtils::SerializeStringIntMap(const std::map<std::string, int32_t>& mp)
+{
+    std::vector<uint8_t> buffer;
+
+    // Store the number of elements in the map
+    uint32_t mapSize = mp.size();
+    buffer.insert(buffer.end(),
+                  reinterpret_cast<uint8_t*>(&mapSize),
+                  reinterpret_cast<uint8_t*>(&mapSize) + sizeof(mapSize));
+
+    // Iterate over the map and serialize each key-value pair
+    for (const auto& it : mp) {
+        SerializeString(it.first, buffer);
+        buffer.insert(buffer.end(),
+                      reinterpret_cast<const uint8_t*>(&it.second),
+                      reinterpret_cast<const uint8_t*>(&it.second) + sizeof(it.second));
+    }
+
+    return buffer;
+}
+
 }
