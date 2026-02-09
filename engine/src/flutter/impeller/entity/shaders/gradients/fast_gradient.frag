CarSmallGuo/flutter_oshm

[Impeller] Fixed an issue where gradient effects on HarmonyOS devices exhibited clipping. With mediump enabled by default, `IPOrderedDither8x8 uint(dest.x)` and `uint(dest.y)` might experience precision loss on some GPU chips.

engine/src/flutter/impeller/entity/shaders/gradients/fast_gradient.frag
@@ -21,6 +21,6 @@ void main() {
   frag_color = IPPremultiply(v_color) * frag_info.alpha;
   // mod operator is not supported in GLES 2.0
 #ifndef IMPELLER_TARGET_OPENGLES
-  frag_color = IPOrderedDither8x8(frag_color, gl_FragCoord.xy);
+  frag_color = PerciseIPOrderedDither8x8(frag_color, gl_FragCoord.x, gl_FragCoord.y);
 #endif  // IMPELLER_TARGET_OPENGLES
 }

engine/src/flutter/impeller/entity/shaders/gradients/linear_gradient_ssbo_fill.frag
@@ -62,5 +62,5 @@ void main() {
   }
 
   frag_color = IPPremultiply(frag_color) * frag_info.alpha;
-  frag_color = IPOrderedDither8x8(frag_color, gl_FragCoord.xy);
+  frag_color = PerciseIPOrderedDither8x8(frag_color, gl_FragCoord.x, gl_FragCoord.y);
 }

engine/src/flutter/impeller/entity/shaders/gradients/radial_gradient_ssbo_fill.frag
@@ -61,5 +61,5 @@ void main() {
   }
 
   frag_color = IPPremultiply(result_color) * frag_info.alpha;
-  frag_color = IPOrderedDither8x8(frag_color, gl_FragCoord.xy);
+  frag_color = PerciseIPOrderedDither8x8(frag_color, gl_FragCoord.x, gl_FragCoord.y);
 }

dgh1818/flutter_engine_ohos

Signed-off-by: zzx89757 <zionjo89757@gmail.com>

engine/src/flutter/impeller/entity/shaders/gradients/fast_gradient.frag
@@ -21,6 +21,6 @@ void main() {
   frag_color = IPPremultiply(v_color) * frag_info.alpha;
   // mod operator is not supported in GLES 2.0
 #ifndef IMPELLER_TARGET_OPENGLES
-  frag_color = IPOrderedDither8x8(frag_color, gl_FragCoord.xy);
+  frag_color = PreciseIPOrderedDither8x8(frag_color, gl_FragCoord.x, gl_FragCoord.y);
 #endif  // IMPELLER_TARGET_OPENGLES
 }

engine/src/flutter/impeller/entity/shaders/gradients/linear_gradient_ssbo_fill.frag
@@ -62,5 +62,5 @@ void main() {
   }
 
   frag_color = IPPremultiply(frag_color) * frag_info.alpha;
-  frag_color = IPOrderedDither8x8(frag_color, gl_FragCoord.xy);
+  frag_color = PreciseIPOrderedDither8x8(frag_color, gl_FragCoord.x, gl_FragCoord.y);
 }

engine/src/flutter/impeller/entity/shaders/gradients/radial_gradient_ssbo_fill.frag
@@ -61,5 +61,5 @@ void main() {
   }
 
   frag_color = IPPremultiply(result_color) * frag_info.alpha;
-  frag_color = IPOrderedDither8x8(frag_color, gl_FragCoord.xy);
+  frag_color = PreciseIPOrderedDither8x8(frag_color, gl_FragCoord.x, gl_FragCoord.y);
 }